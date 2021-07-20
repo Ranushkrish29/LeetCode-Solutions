cc4b98cb69185da46f46020bd4ddf61816b7f0cd
Ranush Krishna <ranush.krishna@gmail.com>
2021-07-20 20:19:54 +0530

    LeetCode Problems

--- 21.Merge Two Sorted Lists.js
+++ 21.Merge Two Sorted Lists.js
@@ -21,8 +21,67 @@ function ListNode(val, next) {
     this.next = (next === undefined ? null : next)
 }
 
-
+//Optimized Implementation    ----- Runtime : O(n) || sapce O(n)
+// Runtime: 
+//      72 ms, faster than 98.62 % of JavaScript online submissions for Merge Two Sorted Lists.
+// Memory Usage:
+//      40.6 MB, less than 31.90 % of JavaScript online submissions for Merge Two Sorted Lists.
 var mergeTwoLists = function (l1, l2) {
+    let currentl1 = l1,
+        currentl2 = l2,
+        dummyheadlist = new ListNode(0),
+        linkedlist = dummyheadlist;
 
+    while (currentl1 !== null && currentl2 !== null) {
+        if (currentl1.val <= currentl2.val) {
+            dummyheadlist.next = new ListNode(currentl1.val);
+            currentl1 = currentl1.next;
+        } else {
+            dummyheadlist.next = new ListNode(currentl2.val);
+            currentl2 = currentl2.next;
+        }
+        dummyheadlist = dummyheadlist.next;
+    }
+    while (currentl1 !== null) {
+        dummyheadlist.next = new ListNode(currentl1.val);
+        currentl1 = currentl1.next;
+        dummyheadlist = dummyheadlist.next;
+    }
+    while (currentl2 !== null) {
+        dummyheadlist.next = new ListNode(currentl2.val);
+        currentl2 = currentl2.next;
+        dummyheadlist = dummyheadlist.next;
+    }
+    return linkedlist.next;
 };
 
+
+
+//------------------------------------------------------------------------------------------
+//helper convertions
+//just for converting the given list to linkedlist and 
+//converting the resultent linked list to list for simplicity 
+function convets(l1, l2) {
+    let dummyheadlist1 = new ListNode(0),
+        linkedlist1 = dummyheadlist1,
+        dummyheadlist2 = new ListNode(0),
+        linkedlist2 = dummyheadlist2;
+    for (let i of l1) {
+        dummyheadlist1.next = new ListNode(i)
+        dummyheadlist1 = dummyheadlist1.next
+    }
+    for (let i of l2) {
+        dummyheadlist2.next = new ListNode(i)
+        dummyheadlist2 = dummyheadlist2.next
+    }
+    //calls the main function 
+    let resultlinkedlist = mergeTwoLists(linkedlist1.next, linkedlist2.next),
+        resultlist = [],
+        current = resultlinkedlist;
+    while (current) {
+        resultlist.push(current.val);
+        current = current.next;
+    }
+    return resultlist;
+}
+console.log(convets(l1 = [1, 2, 4], l2 = [1, 3, 4]))//[ 1, 1, 2, 3, 4, 4 ]
