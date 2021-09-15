940c6c4d89d0cf2f06111db973ee6dab5f6a0d50
Ranush Krishna <ranush.krishna@gmail.com>
2021-09-15 21:24:35 +0530

    LeetCode Problems

---
+++ 54. Spiral Matrix.js
@@ -0,0 +1,60 @@
+/**Given an m x n matrix, return all elements of the matrix in spiral order.
+
+Example 1:
+
+        1  ->   2  ->   3
+                        |
+                        V
+        4  ->   5       6
+        ^               |
+        |               V
+        7  <-   8  <-   9
+
+    Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+    Output: [1,2,3,6,9,8,7,4,5]
+
+Example 2:
+    Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+    Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+
+Constraints:
+    m == matrix.length
+    n == matrix[i].length
+    1 <= m, n <= 10
+    -100 <= matrix[i][j] <= 100 */
+
+
+//Brute Force Implementation    Runtime O(n)
+var spiralOrder = function (matrix) {
+
+    let i = 0,
+        j = 0,
+        nloop = 0,
+        currentLayerPosition = 0,
+        resList = [];
+
+    if (matrix[0].length == 1) {
+        while (nloop < matrix.length * matrix[0].length) {
+            resList.push(matrix[nloop][0]);
+            nloop++;
+        }
+    } else
+        while (nloop < matrix.length * matrix[0].length) {
+            resList.push(matrix[i][j]);
+            if (i == currentLayerPosition && (j == currentLayerPosition || j < matrix[0].length - 1 - currentLayerPosition))
+                if (j + 1 <= matrix[0].length - 1 - currentLayerPosition) j++;
+                else i++;
+            else if (j == matrix[0].length - 1 - currentLayerPosition && (i == currentLayerPosition || i < (matrix.length - 1 - currentLayerPosition)))
+                i++;
+            else if (i == (matrix.length - 1 - currentLayerPosition) && j == matrix[0].length - 1 - currentLayerPosition || j > currentLayerPosition)
+                j--;
+            else if (j == currentLayerPosition && (i == (matrix.length - 1 - currentLayerPosition) || i < (matrix.length - 1 - currentLayerPosition))) {
+                i--;
+                if (i - j == 1) currentLayerPosition++;
+            }
+            nloop++;
+        }
+    return resList;
+};
+console.log(spiralOrder(
+    [[1, 2, 3], [4, 5, 6], [7, 8, 9]]))//[ 1, 2, 3, 6, 9, 8, 7, 4, 5 ]
