d21ee7098f73a34f5703b9061b22a8f9ac751723
Ranush Krishna <ranush.krishna@gmail.com>
2021-09-29 20:48:56 +0530

    LeetCode Problems

--- 59. Spiral Matrix II.js
+++ 59. Spiral Matrix II.js
@@ -55,3 +55,8 @@ var generateMatrix = function (n) {
     return matrix;
 };
 console.log(generateMatrix(5));
+//    [ [ 1,  2,  3,  4, 5],
+//      [16, 17, 18, 19, 6],
+//      [15, 24, 25, 20, 7],
+//      [14, 23, 22, 21, 8],
+//      [13, 12, 11, 10, 9] ]
