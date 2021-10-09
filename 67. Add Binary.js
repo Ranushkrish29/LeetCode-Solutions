6d886ff84d1f634cf76e4e5d8e76b04b6a819a11
Ranush Krishna <ranush.krishna@gmail.com>
2021-10-09 20:17:13 +0530

    LeetCode Problems

---
+++ 67. Add Binary.js
@@ -0,0 +1,36 @@
+/**Given two binary strings a and b, return their sum as a binary string.
+
+Example 1:
+    Input: a = "11", b = "1"
+    Output: "100"
+Example 2:
+    Input: a = "1010", b = "1011"
+    Output: "10101"
+
+Constraints:
+    1 <= a.length, b.length <= 104
+    a and b consist only of '0' or '1' characters.
+    Each string does not contain leading zeros except for the zero itself. */
+
+
+
+//Brute Force Implementation 
+var addBinary = function (a, b) {
+    let sum = BigInt(0),
+        final = "",
+        finalx = '';
+    for (let i = 0; i < a.length; i++)
+        sum += BigInt(Math.pow(2, (a.length - i - 1)) * a[i]);
+    for (let i = 0; i < b.length; i++)
+        sum += BigInt(Math.pow(2, (b.length - i - 1)) * b[i]);
+    if (!sum)
+        return "0";
+    while (sum) {
+        final += sum % BigInt(2);
+        sum = sum / BigInt(2);
+    }
+    for (let i = final.length - 1; i > -1; i--)
+        finalx += final[i];
+    return finalx;
+};
+console.log(addBinary(a = "1010", b = "1011"));//10101
